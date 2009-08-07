6d5af9968711f8ffc2df6312da156329a5a6c3dc
alvaro.veliz <alvaro.veliz@76c0404a-0f31-11de-99c7-9d618077940f>
2009-08-07 09:10:42 +0000

    Limpiando logs

--- trunk/omgt/application/logs/2009-08-05.log.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2009-08-05 01:17:55 -04:00 --- error: Uncaught Kohana_Exception: The requested view, default/index, could not be found in file /home/hypn/www/alpha/omgt/system/core/Kohana.php on line 1162
-2009-08-05 01:18:52 -04:00 --- error: Uncaught Kohana_Exception: The requested view, default/index, could not be found in file /home/hypn/www/alpha/omgt/system/core/Kohana.php on line 1162
-2009-08-05 01:21:14 -04:00 --- error: Uncaught Kohana_Exception: The requested view, default/index, could not be found in file /home/hypn/www/alpha/omgt/system/core/Kohana.php on line 1162
-2009-08-05 01:28:35 -04:00 --- error: Uncaught Kohana_Exception: The requested view, default/index, could not be found in file /home/hypn/www/alpha/omgt/system/core/Kohana.php on line 1162
-2009-08-05 01:28:39 -04:00 --- error: Uncaught Kohana_Exception: The requested view, default/index, could not be found in file /home/hypn/www/alpha/omgt/system/core/Kohana.php on line 1162
