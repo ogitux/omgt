fea07896041815dc1c1634f67a9a1b174b1d47eb
nawdaer <nawdaer@76c0404a-0f31-11de-99c7-9d618077940f>
2009-08-11 05:09:48 +0000

    update v0.1 for the default theme :)

--- trunk/omgt/themes/default/header.php
+++ trunk/omgt/themes/default/header.php
@@ -9,27 +9,25 @@
 <title>OMGT</title>
 
 <link rel="stylesheet" href="/themes/default/css/grid.css" type="text/css" media="screen" charset="utf-8" />
-<link rel="stylesheet" href="/themes/default/css/omgt.css" type="text/css" media="screen" charset="utf-8" />
-<link rel="stylesheet" href="/themes/default/css/overcast/jquery-ui-1.7.2.custom.css" type="text/css" media="screen" charset="utf-8" />	
+<link rel="stylesheet" href="/themes/default/css/site.css" type="text/css" media="screen" charset="utf-8" />
+<link rel="stylesheet" href="/themes/default/css/overcast/jquery-ui-1.7.2.custom.css" type="text/css" media="screen" charset="utf-8" />
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/themes/default/js/omgt.js"></script>
 
 </head>
 <body>
-<div id="main" class="container_16">
-	<div id="header" class="span-16">
-		Open Manager for Game Tournaments
+<div id="main" class="container">
+	<div id="header" class="span-24">
+		<h1>Open Manager for Game Tournaments</h1>
 	</div>
-	<div id="menu" class="span-16">
-		<ol>
+	<div id="menu" class="span-24">
+		<ul>
 			<li>
 				<a href="/">Home</a>
-				<small>Your team page</small>
 			</li>
 			<li>
 				<a href="/admin/">Admin</a>
-				<small>System Configuration</small>
 			</li>
-		</ol>
+		</ul>
 	</div>
